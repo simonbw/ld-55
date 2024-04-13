c53d622b59a70aa03269ffa8f583a0ee49c3fe81
Philip Hann <phann92@gmail.com>
2024-04-13 17:37:08 -0500

    Cleaner map junctions

--- src/ld-55/environment/HallwayLevel.ts
+++ src/ld-55/environment/HallwayLevel.ts
@@ -6,16 +6,24 @@ import { Player } from "../entities/Player.ts";
 import { Wall } from "../entities/Wall.ts";
 
 function addLevelEntities(game: Game) {
-  game.addEntity(new Wall(V(0, 0), V(25, 0)));
-  game.addEntity(new Wall(V(25, 0), V(25, 40)));
-  game.addEntity(new Wall(V(25, 40), V(10, 40)));
+  game.addEntity(new Wall(V(0, 0), V(10, 0)));
+  game.addEntity(new Wall(V(10, 0), V(15, 0)));
+  game.addEntity(new Wall(V(15, 0), V(25, 0)));
+  game.addEntity(new Wall(V(25, 0), V(25, 12)));
+  game.addEntity(new Wall(V(25, 12), V(25, 24)));
+  game.addEntity(new Wall(V(25, 24), V(25, 32)));
+  game.addEntity(new Wall(V(25, 32), V(25, 40)));
+  game.addEntity(new Wall(V(25, 40), V(15, 40)));
+  game.addEntity(new Wall(V(15, 40), V(10, 40)));
 
   game.addEntity(new Door(V(10, 40), V(9, 39)));
   game.addEntity(new Door(V(8, 38), V(9, 39)));
 
   game.addEntity(new Wall(V(8, 38), V(8, 36)));
   game.addEntity(new Wall(V(8, 36), V(0, 36)));
-  game.addEntity(new Wall(V(0, 36), V(0, 0)));
+  game.addEntity(new Wall(V(0, 36), V(0, 24)));
+  game.addEntity(new Wall(V(0, 24), V(0, 12)));
+  game.addEntity(new Wall(V(0, 12), V(0, 0)));
 
   game.addEntity(new Player(V(12.5, 2)));
 
@@ -27,22 +35,28 @@ function addLevelEntities(game: Game) {
 
   game.addEntity(new Wall(V(10, 0), V(10, 1)));
   game.addEntity(new Door(V(10, 1), V(10, 3)));
-  game.addEntity(new Wall(V(10, 3), V(10, 13)));
+  game.addEntity(new Wall(V(10, 3), V(10, 12)));
+  game.addEntity(new Wall(V(10, 12), V(10, 13)));
   game.addEntity(new Door(V(10, 13), V(10, 15)));
-  game.addEntity(new Wall(V(10, 15), V(10, 25)));
+  game.addEntity(new Wall(V(10, 15), V(10, 24)));
+  game.addEntity(new Wall(V(10, 24), V(10, 25)));
   game.addEntity(new Door(V(10, 25), V(10, 27)));
   game.addEntity(new Wall(V(10, 27), V(10, 36)));
   game.addEntity(new Wall(V(10, 36), V(8, 36)));
 
   game.addEntity(new Wall(V(15, 0), V(15, 1)));
   game.addEntity(new Door(V(15, 1), V(15, 3)));
-  game.addEntity(new Wall(V(15, 3), V(15, 13)));
+  game.addEntity(new Wall(V(15, 3), V(15, 12)));
+  game.addEntity(new Wall(V(15, 12), V(15, 13)));
   game.addEntity(new Door(V(15, 13), V(15, 15)));
-  game.addEntity(new Wall(V(15, 15), V(15, 25)));
+  game.addEntity(new Wall(V(15, 15), V(15, 24)));
+  game.addEntity(new Wall(V(15, 24), V(15, 25)));
   game.addEntity(new Door(V(15, 25), V(15, 27)));
-  game.addEntity(new Wall(V(15, 27), V(15, 33)));
+  game.addEntity(new Wall(V(15, 27), V(15, 32)));
+  game.addEntity(new Wall(V(15, 32), V(15, 33)));
   game.addEntity(new Door(V(15, 33), V(15, 35)));
-  game.addEntity(new Wall(V(15, 35), V(15, 40)));
+  game.addEntity(new Wall(V(15, 35), V(15, 36)));
+  game.addEntity(new Wall(V(15, 36), V(15, 40)));
 
   game.addEntity(new Wall(V(10, 36), V(11, 36)));
   game.addEntity(new Door(V(11, 36), V(12.5, 36)));
